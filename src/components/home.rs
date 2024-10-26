7672330cc8ddf858a7cd29a19f597531f3f62451
PandeCode <pandeshawnbenjamin@gmail.com>
2024-10-26 16:46:10 +0000

    returning to project and nix

--- src/components/home.rs
+++ src/components/home.rs
@@ -3,8 +3,21 @@ use yew::prelude::*;
 #[derive(PartialEq, Properties)]
 pub struct HomeProps {}
 
+macro_rules! iter_to_html {
+    ($iter:expr, $html:expr) => {
+        iter.iter().map(|i| <>$html</>).collect::<yew::html::Html>()
+    };
+}
+
 #[function_component]
 pub fn Home(props: &HomeProps) -> Html {
     let HomeProps {} = props;
-    html! { <div class="">{ "Home" }</div> }
+
+    let skills = [vec!["Rust"], vec!["Fullstack"], vec!["C++"], vec!["Python"]];
+    html! { <div class="">
+    {
+        skills.iter().map(|s| s.iter().collect::<yew::html::Html>()).collect::<yew::html::Html>()
+    }
+
+    </div> }
 }
